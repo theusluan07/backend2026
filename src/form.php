8f138387fde54d8261c4ff60cbf94f6fb459c2c4
theusluan07 <theusluan07@uni9.edu.br>
2026-03-19 01:38:20 +0000

    aula - 18/03/26

---
+++ src/form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title> 
+
+</head>
+<body>
+   <h1>Form - 18/03/26</h1>
+   <form methood="post" action="recebe.php">
+   Login<input type="text" id="login" name="login"><br>
+   <br>
+   Senha<input type="password" id="senha" name="senha"><br>
+   <br>
+  <input type="submit" value="Validar">
+</form>
+</body>
+</html>
